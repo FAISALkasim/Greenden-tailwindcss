24d7f7be0a72ab6b8163c73a43c7c44979cd382c
FAISALkasim <mohamedkasim.faisal@gmail.com>
2025-02-17 06:16:31 +0400

    Added js

---
+++ images/index.js
@@ -0,0 +1,7 @@
+//selecting sidenavbar,meuicon
+var sidenav=document.getElementById("sidenav")
+var meuicon=document.getElementById("menuicon")
+sidenav.addEventListener("click",function(){
+
+    sidenav.style.right=0
+})
